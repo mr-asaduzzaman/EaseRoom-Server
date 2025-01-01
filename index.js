7b6e7e1e77c8680027ac73bf3efabdcc579a442b
mr-asaduzzaman <asaduzzamanmix@gmail.com>
2025-01-01 18:18:06 +0600

    middleware Back again

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors({ origin: [
+    'http://localhost:5174',
+    'https://ease-room.web.app',
+    'https://ease-room.firebaseapp.com',
+] }));
 
 
 app.use(express.json());
